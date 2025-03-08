d28d3a6d5bcc959cfafbc232f40c3eb9fcec66ba
vcarnovale <122923089+vcarnovale@users.noreply.github.com>
2025-03-08 12:20:13 -0500

    Fixed some of the text issues with the small card

--- personalwebsite/src/components/Cards.jsx
+++ personalwebsite/src/components/Cards.jsx
@@ -7,12 +7,12 @@ export function Smallcard({ img, title, description, optional }) {
     return (
         <button className="max-w-max min-w-full grid grid-rows-1 grid-cols-[120px_1fr] auto-rows-auto auto-cols-auto gap-5 h-[10%] min-h-[10%] cursor-pointer hover:bg-[#f3f5fd] border-2">
             <img className="h-[60px] w-[60px] justify-self-center place-self-center" src={`${img}`} />
-            <div className="flex flex-row flex-wrap text-wrap">
+            <div className="flex flex-row flex-wrap">
                 <div className="w-[70%] justify-self-start place-self-start grid grid-rows-2 grid-cols-1 auto-rows-auto auto-cols-auto">
-                    <h1 className="text-left">{title}</h1>
+                    <h1 className="text-left text-wrap">{title}</h1>
                     <p1 className="text-left">{description}</p1>
                 </div>
-                <h2 className="text-right justify-self-end place-self-start">{optional}</h2>
+                <h2 className="flex-1 md:text-right not-md:text-left whitespace-nowrap justify-self-end place-self-start">{optional}</h2>
             </div>
         </button>
     )
--- personalwebsite/src/components/Pageheader.jsx
+++ personalwebsite/src/components/Pageheader.jsx
@@ -2,7 +2,7 @@ import { useState } from "react"
 
 export function Dropmen({ dropbuttons, drophref }) {
     return (
-        <div className="lg:hidden absolute bg-black left-0 top-15 w-full">
+        <div className="xl:hidden absolute bg-black left-0 top-15 w-full">
             <nav className="flex flex-col items-center space-y-3" aria-label="mobile">
                 {dropbuttons.map((dbutton) => {
                     return <a key={dbutton} className="cursor-pointer text-white w-full text-center hover:opacity-90">{dbutton}</a>
@@ -16,7 +16,7 @@ export function Dropmen({ dropbuttons, drophref }) {
 function ShowButtons({ menubuttons, menuhref }) {
     return (
         <div>
-            <nav class="hidden lg:block space-x-15 text-lg pr-15 text-white font-semibold" aria-label="main">
+            <nav class="hidden xl:block space-x-15 text-xl pr-15 text-white font-semibold" aria-label="main">
                 {menubuttons.map((mbutton) => {
                     return <a key={mbutton} className="cursor-pointer">{mbutton}</a>
                 })}
@@ -34,10 +34,10 @@ export default function Pageheader({ title, href, buttons }) {
     return (
 
         <div className="bg-black max-w-full w-full mx-auto p-4 flex justify-between items-center fixed ">
-            <button className="text-lg pl-5 font-bold font text-white">{title}</button>
+            <button className="text-xl pl-5 font-bold font text-white">{title}</button>
             <div>
                 {/*We set the button to handle on click if its clicked it opens up our hamburger menu*/}
-                <button id="mobile-open-button" className="lg:hidden focus:outline-none text-2xl cursor-pointer text-white" onClick={() => setOpen(!open)}>&#9776;</button>
+                <button id="mobile-open-button" className="xl:hidden focus:outline-none text-2xl cursor-pointer text-white" onClick={() => setOpen(!open)}>&#9776;</button>
                 {
                     (() => {
                         if (open) {
